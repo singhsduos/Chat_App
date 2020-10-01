7dbb388b4322743c55f7b011dde65d7d4bf533f0
singhsduos <ns9628491678@gmail.com>
2020-10-01 21:56:30 +0530

    all updated

--- lib/Views/conversationScreen.dart
+++ lib/Views/conversationScreen.dart
@@ -154,14 +154,21 @@ class _ChatScreen extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Stack(
-        children: <Widget>[
-          createMessageList(),
+         child: Scaffold(
+           
+        body: Column(
+          children: <Widget>[
+            createMessageList(),
 
-          isDisplaySticker ? emojiContainer() : Container(),
-          // if (isDisplaySticker) Container(child: emojiContainer()) else Container(),
-          createInput(),
-        ],
+            createInput(),  
+          
+            isDisplaySticker  ? Container(child: emojiContainer()) : Container(),
+         
+
+            // if (isDisplaySticker) Container(child: emojiContainer()) else Container(),
+           
+          ],
+        ),
       ),
     );
   }
@@ -428,117 +435,119 @@ class _ChatScreen extends State<ChatScreen> {
 
   Widget emojiContainer() {
     return EmojiPicker(
-      bgColor: Colors.white,
-      indicatorColor: Colors.cyan,
-      rows: 4,
-      columns: 8,
-      onEmojiSelected: (emoji, category) {
-        setState(() {
-          isWriting = true;
-        });
-        messageController.text = messageController.text + emoji.emoji;
-      },
-      recommendKeywords: ['face', 'happy', 'party', 'sad'],
-      numRecommended: 50,
-    );
+        bgColor: Colors.white,
+        indicatorColor: Colors.cyan,
+        rows: 4,
+        columns: 8,
+        onEmojiSelected: (emoji, category) {
+    setState(() {
+      isWriting = true;
+    });
+    messageController.text = messageController.text + emoji.emoji;
+        },
+        recommendKeywords: ['face', 'happy', 'party', 'sad'],
+        numRecommended: 50,
+      );
   }
 
   Widget createInput() {
-    return Container(
-      padding: EdgeInsets.all(5),
+    return Expanded(
+          child: Container(
+        padding: EdgeInsets.all(5),
 
-      alignment: Alignment.bottomCenter,
-      // height: MediaQuery.of(context).size.height,
+        alignment: Alignment.bottomCenter,
+        // height: MediaQuery.of(context).size.height,
 
-      child: Row(
-        children: <Widget>[
-          //image sender icon
-          Material(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 1.0),
-              child: IconButton(
-                icon: Icon(
-                  Icons.image,
-                  size: 30,
-                ),
-                color: Color(0xfff99AAAB),
-                onPressed: getImage,
-              ),
-            ),
-            color: Colors.transparent,
-          ),
-
-          Expanded(
-            child: Stack(
-              alignment: Alignment.centerRight,
-              children: <Widget>[
-                TextField(
-                  controller: messageController,
-                  focusNode: focusNode,
-                  onTap: () => hideEmojiContainer(),
-
-                  style: TextStyle(
-                      color: Colors.white, letterSpacing: 1.0, fontSize: 17),
-                  cursorColor: Colors.cyan,
-                  cursorWidth: 3,
-                  // onChanged: (val) {
-                  //   (val.length > 0 && val.trim() != "")
-                  //       ? setWritingTo(true)
-                  //       : setWritingTo(false);
-                  // },
-                  decoration: InputDecoration(
-                    hintText: "Type a message",
-                    hintStyle: TextStyle(
-                      color: Colors.white60,
-                    ),
-                    border: const OutlineInputBorder(
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(50.0),
-                        ),
-                        borderSide: BorderSide.none),
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                    filled: true,
-                    fillColor: Color(0xfff99AAAB),
+        child: Row(
+          children: <Widget>[
+            //image sender icon
+            Material(
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 1.0),
+                child: IconButton(
+                  icon: Icon(
+                    Icons.image,
+                    size: 30,
                   ),
+                  color: Color(0xfff99AAAB),
+                  onPressed: getImage,
                 ),
-                IconButton(
-                  splashColor: Colors.transparent,
-                  color: Colors.white60,
-                  highlightColor: Colors.transparent,
-                  icon: Icon(Icons.emoji_emotions),
-                  onPressed: () {
-                    if (!isDisplaySticker) {
-                      showEmojiContainer();
-                      hideKeyboard();
-                    } else {
-                      showKeyboard();
-                      hideEmojiContainer();
-                    }
-                  },
-                ),
-              ],
+              ),
+              color: Colors.transparent,
             ),
-          ),
-          Material(
-            child: Container(
-              // margin: EdgeInsets.symmetric(horizontal: 0.0),
-              child: RawMaterialButton(
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(15.0),
-                elevation: 2.0,
-                child: Icon(
-                  Icons.send,
-                  color: Colors.white,
-                ),
-                fillColor: Colors.cyan,
-                splashColor: Colors.transparent,
-                onPressed: () => onSendMessage(messageController.text, 0),
+
+            Expanded(
+              child: Stack(
+                alignment: Alignment.centerRight,
+                children: <Widget>[
+                  TextField(
+                    controller: messageController,
+                    focusNode: focusNode,
+                    onTap: () => hideEmojiContainer(),
+
+                    style: TextStyle(
+                        color: Colors.white, letterSpacing: 1.0, fontSize: 17),
+                    cursorColor: Colors.cyan,
+                    cursorWidth: 3,
+                    // onChanged: (val) {
+                    //   (val.length > 0 && val.trim() != "")
+                    //       ? setWritingTo(true)
+                    //       : setWritingTo(false);
+                    // },
+                    decoration: InputDecoration(
+                      hintText: "Type a message",
+                      hintStyle: TextStyle(
+                        color: Colors.white60,
+                      ),
+                      border: const OutlineInputBorder(
+                          borderRadius: const BorderRadius.all(
+                            const Radius.circular(50.0),
+                          ),
+                          borderSide: BorderSide.none),
+                      contentPadding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      filled: true,
+                      fillColor: Color(0xfff99AAAB),
+                    ),
+                  ),
+                  IconButton(
+                    splashColor: Colors.transparent,
+                    color: Colors.white60,
+                    highlightColor: Colors.transparent,
+                    icon: Icon(Icons.emoji_emotions),
+                    onPressed: () {
+                      if (!isDisplaySticker) {
+                          hideKeyboard();
+                        showEmojiContainer();
+                      } else {
+                        showKeyboard();
+                        hideEmojiContainer();
+                      }
+                    },
+                  ),
+                ],
               ),
             ),
-            // color: Colors.cyan,
-          ),
-        ],
+            Material(
+              child: Container(
+                // margin: EdgeInsets.symmetric(horizontal: 0.0),
+                child: RawMaterialButton(
+                  shape: CircleBorder(),
+                  padding: EdgeInsets.all(15.0),
+                  elevation: 2.0,
+                  child: Icon(
+                    Icons.send,
+                    color: Colors.white,
+                  ),
+                  fillColor: Colors.cyan,
+                  splashColor: Colors.transparent,
+                  onPressed: () => onSendMessage(messageController.text, 0),
+                ),
+              ),
+              // color: Colors.cyan,
+            ),
+          ],
+        ),
       ),
     );
   }
